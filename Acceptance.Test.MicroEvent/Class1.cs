0cda80aba9a2cd5bf468a6cd39c687a054f56279
unknown <herrad.koe@gmail.com>
2013-10-25 20:08:11 +0100

    created project

---
+++ Acceptance.Test.MicroEvent/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acceptance.Test.MicroEvent
+{
+    public class Class1
+    {
+    }
+}
