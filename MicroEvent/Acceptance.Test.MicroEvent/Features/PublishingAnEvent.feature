af4de5cde28dad72ed7b4e3b195e0b70328855ce
herrad <herrad.koe@gmail.com>
2014-02-22 19:18:59 +0000

    Added first acceptance test

---
+++ MicroEvent/Acceptance.Test.MicroEvent/Features/PublishingAnEvent.feature
@@ -0,0 +1,7 @@
+﻿Feature: Publishing an event
+
+Scenario: Publishing an event from service front end triggers a write to the data store
+	Given I have a data store
+	And a service front end
+	When I publish an event via the service
+	Then a write event is triggered
---
+++ MicroEvent/Acceptance.Test.MicroEvent/Steps/PublishingAnEventSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Acceptance.Test.MicroEvent.Steps
+{
+    [Binding]
+    public class PublishingAnEventSteps
+    {
+        [Given(@"I have a data store")]
+        public void GivenIHaveADataStore()
+        {
+            
+        }
+
+        [Given(@"a service front end")]
+        public void GivenAServiceFrontEnd()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I publish an event via the service")]
+        public void WhenIPublishAnEventViaTheService()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"a write event is triggered")]
+        public void ThenAWriteEventIsTriggered()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
