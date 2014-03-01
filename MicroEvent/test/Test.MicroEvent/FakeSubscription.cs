ac5889c8d2510cd679d299a502c21c0feaa003e8
herrad <herrad.koe@gmail.com>
2014-03-01 15:07:49 +0000

    restructured project layout

---
+++ MicroEvent/test/Test.MicroEvent/FakeSubscription.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MicroEvent;
+
+namespace Test.MicroEvent
+{
+    public class FakeSubscription : Subscription
+    {
+        public FakeSubscription(Type eventType) : base(eventType)
+        {
+        }
+
+        public AnEvent LastEvent { get; private set; }
+        public override void Notify(AnEvent anEvent)
+        {
+            LastEvent = anEvent;
+        }
+    }
+}
